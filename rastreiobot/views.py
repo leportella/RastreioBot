f6ba8ecb2120745e4f289ce2343ce4bc283b6af4
Leticia Portella <leportella@gmail.com>
2018-04-15 22:22:16 -0300

    Add users options in add_package views

--- rastreiobot/views.py
+++ rastreiobot/views.py
@@ -11,8 +11,21 @@ async def add_package(message):
     tracking_code = message.text.split()[0]
     content = {'tracking_code': tracking_code}
 
-    if Package.objects.get(tracking_code=tracking_code):
-        return render(message, 'package_already_added.md', content)
+    package_added = False
+    try:
+        package = Package.objects.get(tracking_code=tracking_code)
+    except Package.DoesNotExist:
+        Package(tracking_code=tracking_code, users=[message.user.id]).save()
+        package_added = True
+    else:
+        if message.user.id not in package.users:
+            package.users.append(message.user.id)
+            package_added = True
 
-    Package(tracking_code=tracking_code, users=[]).save()
-    return render(message, 'package_added_successfuly.md', content)
+    if package_added:
+        return render(message, 'package_added_successfuly.md', content)
+
+    return render(message, 'package_already_added.md', content)
+
+
+
